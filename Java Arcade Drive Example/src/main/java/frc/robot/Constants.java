588081ac371e90c4ae208e9229bdfc44970b0858
AnanthV123 <ananth.ven312@gmail.com>
2024-12-24 22:06:18 -0600

    first commit

---
+++ Java Arcade Drive Example/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Constants {
+    // drivetrain motors
+    public class DriveMotors extends Constants {
+        public static final int leftMotorID = 0;
+        public static final int rightMotorID = 1;
+    }
+    public class 
+}
